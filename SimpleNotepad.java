1ec8fb9ca373bc7734a1176b276bf8e0e23c2321
wyy123wyy123wyy123 <taotaobujuedekan@qq.com>
2024-09-17 15:43:16 +0800

    v1.0.0

---
+++ SimpleNotepad.java
@@ -0,0 +1,88 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class SimpleNotepad extends JFrame {
+    private JTextArea textArea;
+    private JMenuBar menuBar;
+    private JMenu fileMenu;
+    private JMenuItem openItem, saveItem, exitItem;
+
+    public SimpleNotepad() {
+        setTitle("记事本");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        textArea = new JTextArea();
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        add(scrollPane, BorderLayout.CENTER);
+
+        menuBar = new JMenuBar();
+        fileMenu = new JMenu("文件");
+        openItem = new JMenuItem("打开");
+        saveItem = new JMenuItem("保存");
+        exitItem = new JMenuItem("退出");
+
+        fileMenu.add(openItem);
+        fileMenu.add(saveItem);
+        fileMenu.addSeparator();
+        fileMenu.add(exitItem);
+        menuBar.add(fileMenu);
+        setJMenuBar(menuBar);
+
+        openItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fileChooser = new JFileChooser();
+                int returnValue = fileChooser.showOpenDialog(null);
+                if (returnValue == JFileChooser.APPROVE_OPTION) {
+                    try {
+                        BufferedReader reader = new BufferedReader(new FileReader(fileChooser.getSelectedFile()));
+                        textArea.read(reader, null);
+                        reader.close();
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        saveItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fileChooser = new JFileChooser();
+                int returnValue = fileChooser.showSaveDialog(null);
+                if (returnValue == JFileChooser.APPROVE_OPTION) {
+                    try {
+                        FileWriter writer = new FileWriter(fileChooser.getSelectedFile());
+                        textArea.write(writer);
+                        writer.close();
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        exitItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new SimpleNotepad().setVisible(true);
+            }
+        });
+    }
+}
